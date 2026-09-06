ce4586db61d4a498363748c25cb9e5c4022f6a3a
0xrp1415 <vyshnavpnair2005@gmail.com>
2026-09-06 15:58:39 +0530

    refactor(dungeon-master): remove dead event schema and unused re-exports

--- backend/src/domains/dungeon-master/schema/index.ts
+++ backend/src/domains/dungeon-master/schema/index.ts
@@ -1,20 +1,9 @@
 export {
-    ZDirection,
-    ZActionIntent,
-    ZTargetType,
-    ZResource,
     ZAction,
     type DmAction,
 } from "./actions/index.js";
 export {
     ZDmRoomView,
-    ZDmExitSnapshot,
-    ZDmPartyMemberSnapshot,
-    ZDmPassageEvent,
     type DmRoomView,
-    type DmExitSnapshot,
-    type DmPartyMemberSnapshot,
-    type DmPassageEvent,
 } from "./room.js";
 export { ZDmVerdict, type DmVerdict } from "./verdict.js";
-export { ZDmGameEvent, type DmGameEvent } from "./event.js";
